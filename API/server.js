1e197d7655a8ffdaeeaa511ff6b9877f72a4ea8c
Justin Legoff <legoff.justin@gmail.com>
2017-01-13 20:40:30 +0100

    register

--- API/server.js
+++ API/server.js
@@ -1,38 +1,46 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var test = require('./ctrl/test');
-var ejs = require('ejs');
+var MongoClient = require('mongodb').MongoClient;
+var ipServer = "137.174.194.70";
+var databaseServer = "mongodb://" + ipServer + ":27017/musictoolkit";
 
 var app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.get('/', function(req,res){
-   
+app.get('/', function (req, res) {
     res.setHeader('Content-Type', 'text/plain');
     res.end('Bienvenue sur l\'accueil du site de MusikToolKit');
-    
+
 });
 
-app.get('/users/:userId/profile', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Vous êtes à la chambre de l\'étage n°' + req.params.userId);
+app.get('/register', function (req, res) {
+    var user = {
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        register_date: new Date(),
+        email: req.body.email,
+        password: sha1(req.body.password)
+    };
+
+    MongoClient.connect(databaseServer, function (err, db) {
+        if (!err) {
+            db.collection("users").find({email: user.email}).toArray(function (error, results) {
+                if (error) throw error;
+                if(results.length == 0){
+                    db.collection("users").insert(user, function(error, results){
+                        
+                    });
+                }                
+            });
+        }
+    });
+
 });
 
-app.get('/counter/:nulber', function(req,res){
-    var users = ['Edouard','Pierre','Pierre','Matthieu','Nadège'];
-    res.status(200).send('counter.ejs', {counter : req.params.number, users : users});
-});
-
-app.use(function(req,res,next){
-    res.setHeader('Content-Type', 'text/plain');
-    res.status(404).send('Désolé, cette page est introuvable');
-});
-
-//Test for POST method
-//app.use('/test', test);
-
-app.listen(8080);
+app.listen(3333);
 
 console.log('Server running...');
